531873f0ca508bd0cc2cdb742bd2e595be52ad34
Bapan Biswas <babiswas@adobe.com>
2022-05-08 06:09:59 +0530

    commited

--- Post/serializer.py
+++ Post/serializer.py
@@ -3,11 +3,15 @@ from .models import Content
 
 
 class TagSerializer(serializers.Serializer):
+    '''This serializer is used to serialize tag models'''
+
     id = serializers.IntegerField(read_only=True)
     tag_name=serializers.CharField(max_length=100)
 
 
 class ContentSerializer(serializers.ModelSerializer):
+    '''This serializer is used to serialize content'''
+
     id = serializers.IntegerField(read_only=True)
     comments=serializers.StringRelatedField(many=True)
     class Meta:
